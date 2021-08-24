8837b3ea175fc37c7cb0cdfde2766f60212d9335
Cigdem Turkmen (BilgeAdam Ogrenci) <cigdem.turkmen@baogrenci.com>
2021-08-24 13:23:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace helloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı giriniz");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("soyad gir");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("Adınız: {0} Soyadınız: {1}", ad, soyad);
+            Console.WriteLine($"Adınız: {ad} Soyadınız: {soyad}");
+
+   
+        }
+    }
+}
